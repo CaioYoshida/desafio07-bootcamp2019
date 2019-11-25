66c0b8f33808ee6add60dd7f2c969d3cc2247717
Caio Yoshida <caio.yoshida@yahoo.com.br>
2019-11-25 10:52:13 -0300

    React Navigation Configuration

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import 'react-native-gesture-handler';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000',
-  },
-  welcome: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#FFF',
-    textAlign: 'center',
-    margin: 0,
-  },
-});
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Hello Rocketshoes</Text>
-    </View>
+    <>
+      <Routes />
+    </>
   );
 }
